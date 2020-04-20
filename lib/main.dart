3245dc14576e1de30df92a0514c618f09a05785c
Leman Fotera <lemanfoetra@gmail.com>
2020-04-20 21:40:28 +0700

    Styling & Theming

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,28 @@ import './categories_screen.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
- 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Meals',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.teal,
+        accentColor: Colors.amber,
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        fontFamily: 'Raleway',
+        textTheme: ThemeData.light().textTheme.copyWith(
+              body1: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              body2: TextStyle(
+                color: Color.fromRGBO(20, 51, 51, 1),
+              ),
+              title: TextStyle(
+                fontSize: 20,
+                fontFamily: 'RobotoCondensed',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
       ),
       home: Home(),
     );
@@ -28,6 +43,3 @@ class Home extends StatelessWidget {
     );
   }
 }
-
-
-
