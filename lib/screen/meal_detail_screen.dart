1d87a777ffaf554b99e00dbe7d9f39d00dd9df45
Leman Fotera <lemanfoetra@gmail.com>
2020-04-30 08:08:41 +0700

    Finishing the Meal Detail Page

--- lib/screen/meal_detail_screen.dart
+++ lib/screen/meal_detail_screen.dart
@@ -1,19 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:meals/dummy_data.dart';
 
 class MealDetailSceen extends StatelessWidget {
-  
   static const routeName = '/meal-detail';
 
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
 
+    var dataMeal = DUMMY_MEALS.firstWhere((data) {
+      return data.id == mealId;
+    });
+
+    List<Widget> listStep = [];
+    for (var i = 0; i < dataMeal.steps.length; i++) {
+      listStep.add(Column(
+        children: <Widget>[
+          ListTile(
+            leading: CircleAvatar(
+              child: Text('# ${(i + 1)}'),
+            ),
+            title: Text(dataMeal.steps[i]),
+          ),
+          Divider(),
+        ],
+      ));
+    }
+
     return Scaffold(
       appBar: AppBar(
-        title: Text("$mealId"),
+        title: Text(dataMeal.title),
       ),
-      body: Container(
-        child: Text("$mealId"),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Stack(
+              fit: StackFit.expand,
+              children: <Widget>[
+                Image.network(
+                  dataMeal.imageUrl,
+                  fit: BoxFit.cover,
+                ),
+                Positioned(
+                  bottom: 20,
+                  left: 20,
+                  child: Container(
+                    color: Colors.black45,
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      dataMeal.title,
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(bottom: 20, top: 20),
+            child: Text(
+              'Ingredients',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+          ...dataMeal.ingredients.map((data) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(right: 20, left: 20),
+                  child: Text(
+                    data,
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20, right: 20),
+                  child: Divider(),
+                )
+              ],
+            );
+          }).toList(),
+          Container(
+            padding: EdgeInsets.only(top: 20, bottom: 20),
+            child: Text(
+              'Steps',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.title,
+            ),
+          ),
+
+          ...listStep,
+
+          // ...dataMeal.steps.map((data) {
+          //   return Column(
+          //     crossAxisAlignment: CrossAxisAlignment.start,
+          //     children: [
+          //       Container(
+          //         padding: EdgeInsets.only(left: 20, right: 20),
+          //         child: Text(
+          //           data,
+          //           style: TextStyle(fontSize: 18),
+          //         ),
+          //       ),
+          //       Padding(
+          //         padding: EdgeInsets.only(left: 20, right: 20),
+          //         child: Divider(),
+          //       )
+          //     ],
+          //   );
+          // }).toList(),
+        ],
       ),
     );
   }
